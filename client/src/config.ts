867921c802c5db7a6429cf07c54e21ea9485cf51
fpt-phongnx8 <phongnx8@fpt.com>
2023-11-27 23:51:54 +0900

    First commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'co8a8j95e2'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-2of8i4hgdvlqbs7v.us.auth0.com',    // Domain from Auth0
+  clientId: 'mAQAdDbNkHfmgX6mQHR3ijpGgNEgsPNw',  // Client id from an Auth0 application
+  callbackUrl: 'http://localhost:3000/callback'
+}
